99b7ea43f4aeee01d1ebd31bf251f0d42aa4a487
Bogdan Nezhyvyi <bnezhivoy@gmail.com>
2024-05-20 12:35:46 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Contacts from '@/components/Contacts';
+
+export default function Home() {
+    return (
+        <>
+            <Contacts />
+        </>
+    );
+}
---
+++ components/Contacts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactList from '@/components/contacts/contactList';
+
+const Contacts: React.FC = () => {
+    return (
+        <div>
+            <ContactList />
+        </div>
+    );
+};
+
+export default Contacts;
